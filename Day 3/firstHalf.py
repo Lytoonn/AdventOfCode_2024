c5fb51cf35c29926393d5d0c2bc54e1681ffc092
Lytoonn <lytoonytgg@gmail.com>
2024-12-05 16:54:24 +0000

    first half day3

---
+++ Day 3/firstHalf.py
@@ -0,0 +1,26 @@
+import re
+
+f = open('input.txt', 'r').read()
+
+muls = []
+pairsDigits = []
+
+pattern = r'mul\(\d+,\d+\)'
+digits = r'\d+,\d+'
+
+muls = re.findall(pattern, f)
+pairsDigits = [re.findall(digits, i) for i in muls]
+
+list_ = []
+
+for pairs in pairsDigits:
+    for subPair in pairs:
+        y = subPair.split(',')
+        nums = [int(x) for x in y]
+        list_.append(nums)
+
+soma = 0
+for num in list_:
+    soma += num[0]*num[1]
+    
+print(soma)
